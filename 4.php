bd65d0764786108b4ec00d7c0fd23d16c42e67b4
Dima <Dmitrii.yakovlev@mail.ru>
2014-06-17 13:09:35 +0200

    clear

--- 4.php
+++
@@ -1,5 +0,0 @@
-<fieldset>
-<legend>Пол</legend>
-Муж. <input type="radio" name="sex" value="1">
-Жен. <input type="radio" name="sex" value="2">
-</fieldset>
--- 6.php
+++
@@ -1,4 +0,0 @@
-<?php
-$A=array( int, float , float  );
-echo int;
-?>
