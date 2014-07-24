9dac7d04ebfe7bb41e2138dd2938f8101224a287
Dima <Dmitrii.yakovlev@mail.ru>
2014-07-24 07:51:03 +0200

    Perecodiroval

--- RegInForum2.php
+++ RegInForum2.php
@@ -1,6 +1,7 @@
     <?php
     $email = $_POST['email'];
     $password = $_POST['password'];
+    mysql_query("SET NAMES 'utf-8'");
     mysql_connect("localhost", "root", "") or die (mysql_error ());
     mysql_select_db("test") or die(mysql_error());
       $sql = "SELECT * FROM regist WHERE `password`='".$password."' AND `email`='".$email."'";
@@ -14,7 +15,13 @@
           echo "Не Правильное мыло или пароль";                   
       }
       
-    
-      
+    $arr_main=array(); 
+    $temp_array=array();
+    $temp_array['password']=$row['password'];
+    $temp_array['email']=$row['email'];
+    $arr=array_push($temp_array,$arr_main);
+
+    echo $arr;
+    print_r($arr_main);  
     mysql_close();
     ?>
