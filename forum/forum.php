e6b17a28fb0c8bf015353d7700c7865f41dffc6e
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-02 01:52:31 +0200

    need help to base

--- forum/forum.php
+++ forum/forum.php
@@ -21,36 +21,7 @@ echo "Views=". $_SESSION['views'];
         <title>Forum</title>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     </head>
-    <body>
-<?php
-$day_b=array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31);
-$month=array("January","February","March","April","May","June","July","August","September","October","November","December");
-$location=array("New York","Las Vegas","Washington");
-$year=array(2014,2013,2012,2011,2009,2008,2007,2006,1990);
-$submit=( isset($_GET['submitted']) ? $_GET['submitted'] : '' );
-$yearf=( isset($_GET['year']) ? $_GET['year'] : '' );
-$monthf=( isset($_GET['month']) ? $_GET['month'] : '' );
-$dayf=( isset($_GET['day_b']) ? $_GET['day_b'] : '' );
-$dmy=$day_b["$dayf"].$month["$monthf"].$year["$yearf"];
-$email=( isset($_GET['email']) ? $_GET['email'] : '' );
-$password=( isset($_GET['password']) ? $_GET['password'] : '' );
-$name=( isset($_GET['name']) ? $_GET['name'] : '' );
-$lname=( isset($_GET['lname']) ? $_GET['lname'] : '' );
-$nname=( isset($_GET['nname']) ? $_GET['nname'] : '' );
-$sex=( isset($_GET['sex']) ? $_GET['sex'] : '' );
-$loc=( isset($_GET['location']) ? $_GET['location'] : '' );
-$action=( isset($_GET['action']) ? $_GET['action'] : '' );
-$f1=( isset($_GET['f1']) ? $_GET['f1'] : '' );
-?>          
-<?php
-    $screen=1;
-if (isset($submit) && $submit==1 ) {
-    $screen=3; 
-if ( empty($name)||empty($lname)||empty($password)||empty($nname)||empty($email) ) {
-    $screen=2;
-}
-}
-?>
+    <body>         
 <?php 
 $action='';
  
@@ -63,7 +34,5 @@ if (isset($_GET['action']) && file_exists($_GET['action'].".php")) {
 	include_once 'login.phtml';
 }
 ?>
- 
- 
     </body>
 </html>
--- forum/forum_temp.php
+++ forum/forum_temp.php
@@ -1,4 +1,24 @@
-<form action="login.php" method="post" >
+<?php
+$day_b=array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31);
+$month=array("January","February","March","April","May","June","July","August","September","October","November","December");
+$location=array("New York","Las Vegas","Washington");
+$year=array(2014,2013,2012,2011,2009,2008,2007,2006,1990);
+$submit=( isset($_GET['submitted']) ? $_GET['submitted'] : '' );
+$yearf=( isset($_GET['year']) ? $_GET['year'] : '' );
+$monthf=( isset($_GET['month']) ? $_GET['month'] : '' );
+$dayf=( isset($_GET['day_b']) ? $_GET['day_b'] : '' );
+$dmy=( isset($_GET['$dayf']) ? $_GET['$dayf'] : '' ).( isset($_GET['$monthf']) ? $_GET['$monthf'] : '' ).( isset($_GET['yearf']) ? $_GET['yearf'] : '' );
+$email=( isset($_GET['email']) ? $_GET['email'] : '' );
+$password=( isset($_GET['password']) ? $_GET['password'] : '' );
+$name=( isset($_GET['name']) ? $_GET['name'] : '' );
+$lname=( isset($_GET['lname']) ? $_GET['lname'] : '' );
+$nname=( isset($_GET['nname']) ? $_GET['nname'] : '' );
+$sex=( isset($_GET['sex']) ? $_GET['sex'] : '' );
+$loc=( isset($_GET['location']) ? $_GET['location'] : '' );
+$action=( isset($_GET['action']) ? $_GET['action'] : '' );
+$f1=( isset($_GET['f1']) ? $_GET['f1'] : '' );
+?>
+<form action="login.phtml" method="get" >
     <table border="1" style="margin:auto;" bordercolor="#EEEEEE">
         <tr>
             <td>Email</td>
@@ -49,7 +69,7 @@
         <tr>
             <td>Sex</td>
             <td id="sext"><input id="male" name="sex" type="radio" value="male" />Male
-                             <input id="female" name="sex" type="radio" value="female" />Female
+                          <input id="female" name="sex" type="radio" value="female" />Female
             </td>
         </tr>
         <tr>
@@ -70,25 +90,31 @@
                 <input name="submitted" type="hidden" value="1"/><input id="js_button" type="submit" value="Sign Up"/>
             </td>
         </tr>
-    </table>
-              <?php  } elseif ($screen==2) { ?>                
-              <?php }else { ?>                        
-                <?php           
+    </table>                                                                  
+        <?php           
             mysql_connect("localhost", "root", "") or die (mysql_error ());
-            mysql_select_db("test") or die(mysql_error());   
-             $strSql=' INSERT INTO regist 
-             (email,password,name,last_name,nickname,date_of_birth,sex,location)   VALUES 
-             ("'.$email.'", "'.$password.'", "'.$name.'", "'.$lname.'", "'.$nname.'", "'.$dmy.'", "'.$sex.'", "'.$location[$loc].'")';
-            $result=mysql_query($strSql); 
+            mysql_select_db("test") or die(mysql_error());
+                $query = "SELECT * FROM regist WHERE `email`='".$email."'";
+                $sql = mysql_query($query) or die(mysql_error());
+                    if (mysql_num_rows($sql) < 0)
+                    {
+                        echo '<script type="text/javascript">alert("Mailbox is already Busy");</script>';
+                        echo 'Email busy!';
+                        echo '<script type="text/javascript">window.location = "forum.php";</script>';
+                    }
+                $strSql=' INSERT INTO regist 
+                (email,password,name,last_name,nickname,date_of_birth,sex,location)  
+                VALUES 
+                ("'.$email.'", "'.$password.'", "'.$name.'", "'.$lname.'", "'.$nname.'", "'.$dmy.'", "'.$sex.'", "'.$loc.'")';
+                $result=mysql_query($strSql); 
             if ($strSql){
                 echo "SENCS!!!!";
                 echo "$strSql";
             }
-           mysql_close();
-?>                                                   
-   <?php } ?> 
+            mysql_close();
+        ?>                                                    
 </form>
-<form action="login.php">
+<form action="login.phtml">
     <div align="center">
         <button type="submit">Button to Login</button>
     </div>
