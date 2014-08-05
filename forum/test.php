af4b717c57989a29ae59a9b04a311aa0c58085af
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-05 23:10:52 +0200

    uchu stroki

--- forum/test.php
+++ forum/test.php
@@ -1,12 +1,12 @@
 <?php 
 $action='';
-include_once 'variables.php';
-if(isset($_GET['action']) && file_exists($_GET['action'].".php")){
+include 'variables.php';
+if(isset($_GET['action']) && file_exists($_GET['action']).".php"){
     $action=$_GET['action'];
     include_once $action.".php";
-}elseif(isset ($_GET['action']) && !file_exists($_GET['action'].".php")){
-    include_once 'not_found.phtml';
+}elseif(isset ($_GET['action']) && !file_exists($_GET['action']).".php"){
+    include_once 'not_found.phtml';   
 }else {
-    include_once 'forum.phtml';
+    include_once 'login.phtml';
 }
 ?>
