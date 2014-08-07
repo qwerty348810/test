3f9a1546d622e462f6c5a5cd582768e0d672ab20
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-07 07:15:40 +0200

    forum

--- forum/index.php
+++ forum/index.php
@@ -10,7 +10,6 @@ $_SESSION['views']=1;
 }
 ?>
 <!DOCTYPE html>
-
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="" lang="" dir="">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
---
+++ forum/page_not.php
@@ -0,0 +1,8 @@
+<body>
+    <div align="center">
+<h1>Page not created</h1>
+<a href="community.phtml">Go Back</a>
+<p><img  src="http://rpwiki.ru/images/thumb/8/88/%D0%93%D0%BD%D0%BE%D0%BC_%D1%80%D0%B5%D0%BC%D0%BE%D0%BD%D1%82%D0%BD%D0%B8%D0%BA.jpg/470px-%D0%93%D0%BD%D0%BE%D0%BC_%D1%80%D0%B5%D0%BC%D0%BE%D0%BD%D1%82%D0%BD%D0%B8%D0%BA.jpg"></p>
+    </div>
+</body>
+
