4d396e2b707bf44ef97fe1547a0b130c146bb4c9
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-07 03:01:06 +0200

    fix file name

---
+++ forum/login_check.php
@@ -0,0 +1,19 @@
+<?php include_once 'variables.php'; ?>
+<?php   
+    $mysql_con= "SELECT * FROM regist WHERE `email`='".$email."'";
+    $sql = mysql_query($mysql_con) or die(mysql_error());
+        if (mysql_num_rows($sql) > 0) {
+            echo '<script type="text/javascript">alert("Mailbox is already Busy");</script>';
+            echo 'Email busy!';
+            echo '<script type="text/javascript">window.location = "forum.php";</script>';
+        }
+            $strSql=' INSERT INTO regist 
+            (email,password,name,last_name,nickname,date_of_birth,sex,location)  
+            VALUES 
+            ("'.$email.'", "'.$password.'", "'.$name.'", "'.$lname.'", "'.$nname.'", "'.$dmy.'", "'.$sex.'", "'.$loc.'")';
+            $result=mysql_query($strSql); 
+        if ($strSql){
+            echo "Thank you for registering!!!!";          
+        }
+    mysql_close();
+?>               
