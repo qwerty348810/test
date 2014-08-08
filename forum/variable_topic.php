5524a18e3c7fdd5e8bdd4a6731ba0c546a7dc7b0
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-09 00:41:50 +0200

    ups

--- forum/variable_topic.php
+++ forum/variable_topic.php
@@ -2,7 +2,7 @@
 $mysql_con= "SELECT * FROM topic " ;
     $sql = mysql_query($mysql_con) or die(mysql_error());        
             $strSql=' INSERT INTO topic 
-            (,text)  
+            (text)  
             VALUES 
             ("'.$texttopic.'")';
             $result=mysql_query($strSql); 
