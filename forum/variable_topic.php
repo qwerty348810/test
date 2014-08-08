aa31ccff76759eab6a1096d62fcd25a5f589bf42
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-09 01:18:16 +0200

    table

--- forum/variable_topic.php
+++ forum/variable_topic.php
@@ -2,12 +2,13 @@
 $mysql_con= "SELECT * FROM topic " ;
     $sql = mysql_query($mysql_con) or die(mysql_error());        
             $strSql=' INSERT INTO topic 
-            (text)  
+            (title)  
             VALUES 
             ("'.$texttopic.'")';
             $result=mysql_query($strSql); 
         if ($strSql){
-            echo "!!!!!!!!!!";            
+            echo "!!!!!!!!!!";
+            echo "<table class='general'><tr><td class='left'>.$textopic.</td></tr></table>";
         }            
             echo $texttopic;
     mysql_close();
