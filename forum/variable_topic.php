b3113ca6eeeafd0f466e58b89bff2712d976e1e0
Alex Lukyanau <cybergomel@gmail.com>
2014-08-08 20:22:46 -0400

    Update variable_topic.php

--- forum/variable_topic.php
+++ forum/variable_topic.php
@@ -1,24 +1,16 @@
 <?php
-$mysql_con= "SELECT * FROM topic " ;
-    $sql = mysql_query($mysql_con) or die(mysql_error());        
-            
-            
-            $strSql2='SELECT * FROM topic WHERE title';
-            $rs=mysql_query($strSql2);
-<<<<<<< HEAD
-            while($row = mysql_fetch_assoc($rs)) {
-                print_r($row);                               
-                echo "<table><tr><td>'".$title."'</td></tr></table>";               
-	}       
-        print_r($row);
-=======
+	$sql= "SELECT * FROM topic " ;
+    	$result = mysql_query($mysql_con) or die(mysql_error());        
+ 
+	echo "<table>";
             while($row = mysql_fetch_assoc($result)) {
                 print_r($row);
                 
-                echo "<table><tr><td>'".$title."'</td></tr></table>";               
+                 echo '<tr><td>'.$row['News'].'</td></tr>';               
 		}
-         
-        
->>>>>>> fa1c6d942fa7b4e132f1db64af39d28888a34f7d
+	echo "</table>";
+
+
+
     mysql_close();
 ?>
