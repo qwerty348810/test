0b0828673f095018a6a9b153bb77077c31f97816
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-09 08:37:27 +0200

    ++

--- forum/variable_topic.php
+++ forum/variable_topic.php
@@ -1,11 +1,11 @@
 <?php
-    $mysql_con= "SELECT * FROM topic WHERE parent_id='0'" ;   
+    $mysql_con= "SELECT * FROM topic WHERE parent_id='".$rows['id']."'" ;   
     $result = mysql_query($mysql_con) or die(mysql_error());        
  
 	echo "<table class='general'>";
         while($row = mysql_fetch_assoc($result)) {
             print_r($row);
-            echo '<a href='.'community.php?action='.$row['id'].'><tr><td class="color">'.$row['title'].'</td></tr></a>';               
+            echo '<tr><td class="color">'.$row['title'].'</td></tr></a>';               
         }
         echo "</table>";       
    mysql_close();
