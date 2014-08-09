68cf931d46003a486d942c32b256a50e5479ddea
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-09 03:12:47 +0200

    file

--- forum/variable_topic.php
+++ forum/variable_topic.php
@@ -1,11 +1,15 @@
 <?php
 $mysql_con= "SELECT * FROM topic " ;
-    $sql = mysql_query($mysql_con) or die(mysql_error());                               
+    $sql = mysql_query($mysql_con) or die(mysql_error());        
+            
+            
             $strSql2='SELECT * FROM topic WHERE title';
             $rs=mysql_query($strSql2);
             while($row = mysql_fetch_assoc($rs)) {
                 print_r($row);                               
                 echo "<table><tr><td>'".$title."'</td></tr></table>";               
-	}        
+	}
+        
+        print_r($row);
     mysql_close();
 ?>
