45e6413d162552f571dd68d627152bcd84b19154
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-09 10:21:57 +0200

    +

--- forum/variable_topic.php
+++ forum/variable_topic.php
@@ -5,7 +5,7 @@
 	echo "<table class='general'>";
         while($row = mysql_fetch_assoc($result)) {
             print_r($row);
-            echo '<a href=community.php?action=variable_topic&texttopic='.$row['id'].'><tr><td class="color">'.$row['title'].'</td></tr></a>';               
+            echo '<a href=community.php?action=variable_topic&parent_id='.$row['id'].'><tr><td class="color">'.$row['title'].'</td></tr></a>';               
         }
         echo "</table>";
         echo $_GET['parent_id'];
