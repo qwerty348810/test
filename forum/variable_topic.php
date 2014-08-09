629f0ec4077cd8738d7de6071c2d998f1ef9da70
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-09 03:02:15 +0200

    +

--- forum/variable_topic.php
+++ forum/variable_topic.php
@@ -1,19 +1,13 @@
 <?php
 $mysql_con= "SELECT * FROM topic " ;
-    $sql = mysql_query($mysql_con) or die(mysql_error());        
-            $strSql=' INSERT INTO topic 
-            (title)  
-            VALUES 
-            ("'.$texttopic.'")';
-            $result=mysql_query($strSql);
+    $sql = mysql_query($mysql_con) or die(mysql_error());                               
             $strSql2='SELECT * FROM topic WHERE title';
             $rs=mysql_query($strSql2);
-            while($row = mysql_fetch_array($rs)) {
-                print ($row);
-                
+            while($row = mysql_fetch_assoc($rs)) {
+                print_r($row);                               
                 echo "<table><tr><td>'".$title."'</td></tr></table>";               
 	}
-         print ($row);
+        
         
     mysql_close();
 ?>
