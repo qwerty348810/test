f52dbbdfad4b2d9e5127822e79a7fd168d925299
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-09 07:48:09 +0200

    child

--- forum/variable_topic.php
+++ forum/variable_topic.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
     $mysql_con= "SELECT * FROM topic " ;   
     $result = mysql_query($mysql_con) or die(mysql_error());        
  
@@ -8,30 +7,6 @@
             print_r($row);
             echo '<tr><td class="color">'.$row['title'].'</td></tr>';               
         }
-        echo "</table>";
-        
-        $strSql=' INSERT INTO topic 
-            (text)  
-            VALUES 
-            ("'.$texttopic.'")';
-                
-        $res=mysql_query($strSql); 
-        
-        print_r($texttopic);
-=======
-	$sql= "SELECT * FROM topic " ;
-    	$result = mysql_query($mysql_con) or die(mysql_error());        
- 
-	echo "<table>";
-            while($row = mysql_fetch_assoc($result)) {
-                print_r($row);
-                
-                 echo '<tr><td>'.$row['News'].'</td></tr>';               
-		}
-	echo "</table>";
-
-
->>>>>>> b3113ca6eeeafd0f466e58b89bff2712d976e1e0
-
-    mysql_close();
+        echo "</table>";       
+   mysql_close();
  ?>
