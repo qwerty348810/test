22fc3528eed36baf5d1be42bb2c818ce12d381ba
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-11 03:44:59 +0200

    trable

--- forum/variable_topic.php
+++ forum/variable_topic.php
@@ -1,10 +1,8 @@
 <?php
     $mysql_con= "SELECT * FROM topic WHERE parent_id='".$parent_id."'" ;   
-    $result = mysql_query($mysql_con) or die(mysql_error());        
- 
+    $result = mysql_query($mysql_con) or die(mysql_error());         
 	echo "<table class='general'>";
-        while($row = mysql_fetch_assoc($result)) {
-            print_r($row);
+        while($row = mysql_fetch_assoc($result)) {           
             echo '<tr><td class="color"><a href=community.php?action=variable_topic&parent_id='.$row['id'].'>'.$row['title'].'</a></td></tr>';               
         }
         echo "</table>";        
