72fcc30c75c438f5197aa66eb704f879cd58ff22
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-12 05:08:35 +0200

    remember 2

--- forum/variable_coment.php
+++ forum/variable_coment.php
@@ -3,11 +3,8 @@
         (text,parent_id)  
         VALUES 
         ("'.$textcoment.'","'.$parent_id.'")';               
-    $result3=mysql_query($mysql_con3) or die(mysql_error()); 
-        if ($strSql){
-            echo "!!!!!!!!!!";
-            echo $strSql ;
-            print_r ($row);
-        }                        
+    $result3=mysql_query($mysql_con3) or die(mysql_error());               
+        echo $parent_id ;
+        print_r ($row);
     mysql_close();
 ?>
