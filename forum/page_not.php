be19fa266a6b8c049ae7a1ca3c40391e51efc2ff
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-13 03:26:27 +0200

    sweep

--- forum/page_not.php
+++
@@ -1,8 +0,0 @@
-<body>
-    <div align="center" class="register">
-        <h1><font color="red">Page not created</font></h1>
-        <a href="community.phtml">Go Back</a>
-        <p><img  src="http://rpwiki.ru/images/thumb/8/88/%D0%93%D0%BD%D0%BE%D0%BC_%D1%80%D0%B5%D0%BC%D0%BE%D0%BD%D1%82%D0%BD%D0%B8%D0%BA.jpg/470px-%D0%93%D0%BD%D0%BE%D0%BC_%D1%80%D0%B5%D0%BC%D0%BE%D0%BD%D1%82%D0%BD%D0%B8%D0%BA.jpg"></p>
-    </div>
-</body>
-
