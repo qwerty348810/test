bba699dcdcbc417821d4fec8571b65dc8e3ba440
Alex Lukyanau <cybergomel@gmail.com>
2014-08-13 00:46:22 -0400

    Messages

--- sashas/messages.php
+++ sashas/messages.php
@@ -1,20 +1,15 @@
 <?php
 
 $parent_id=(isset($_GET['parent_id']) ? $_GET['parent_id'] : 0);
-
-$sql="SELECT * FROM topic WHERE parent_id=".$parent_id;
+ 
+$sql="SELECT * FROM messages WHERE parent_id=".$parent_id;
 
 $result=mysql_query($sql,$mysql_con);
 
-$topics=array();
+$messages=array();
 
 while ($row = mysql_fetch_assoc($result)) {
- 	array_push($topics, $row);
+	array_push($messages, $row);
 }
-
-$forum_action='forum';
-
-if ($parent_id) 
-	$forum_action='messages';
 
 include_once $action.'.phtml';
