c11370f78cc06ce11eae6f36f24907218c67b292
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-13 06:44:51 +0200

    renaming messegessend

--- forum/messages_send.php
+++ forum/messages_send.php
@@ -1,9 +1,9 @@
 <?php
-    $msql_messagessend=' INSERT INTO messages 
+    $msql_messages_send=' INSERT INTO messages 
         (messages,parent_id)  
         VALUES 
         ("'.$messages.'","'.$parent_id.'")';               
-    $result_messegessend=mysql_query($msql_messagessend) or die(mysql_error());                             
+    $result_messeges_send=mysql_query($msql_messages_send) or die(mysql_error());                             
     mysql_close();
     echo "</table>";
     echo "<table class='general'>";
--- forum/topic.php
+++ forum/topic.php
@@ -4,7 +4,7 @@
         echo "<table class='general'>";
         echo '<tr><td class="color">MESSAGES</td></tr>';
         echo "<p>Write a message<Br><textarea name='messages' cols='25' rows='3' ></textarea></p>
-        <input type='hidden'  name='action' value='messagessend' />
+        <input type='hidden'  name='action' value='messages_send' />
         <input type='hidden'  name='parent_id' value='$parent_id' />
         <input class='b2' align='middle' type='submit' value='NEW MESSAGE'/>";
         while($row = mysql_fetch_assoc($result_messages)) {
