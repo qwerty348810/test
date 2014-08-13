c3e7597407e8121844c4f073e0a14a5d31caf55f
Alex Lukyanau <cybergomel@gmail.com>
2014-08-13 01:23:02 -0400

    class

--- sashas/messages.php
+++ sashas/messages.php
@@ -6,7 +6,8 @@ if (isset($_POST['parent_id'])) {
 	$post_message_text=$_POST['message_text'];
 	
 	$sql="INSERT INTO messages (messages, parent_id) VALUES ('".$post_message_text."',".$post_parent_id.") ";
-	mysql_query($sql,$mysql_con);
+	$db_connection->save($sql);
+	/*mysql_query($sql,$mysql_con);*/
 	$message_added="<center>Message Added! Thanks</center>";
 	 
 }
@@ -15,7 +16,9 @@ $parent_id=(isset($_GET['parent_id']) ? $_GET['parent_id'] : 0);
  
 $sql="SELECT * FROM messages WHERE parent_id=".$parent_id;
 
-$result=mysql_query($sql,$mysql_con);
+/* $result=mysql_query($sql,$mysql_con); */
+
+$result=$db_connection->query($sql);
 
 $messages=array();
 
--- sashas/variables.php
+++ sashas/variables.php
@@ -1,7 +1,10 @@
 <?php
 
+include_once 'db_class.php';
+
 $template_dir="template/";
 
+/*
 $db_host='localhost';
 $db_username='root';
 $db_name='test';
@@ -15,3 +18,6 @@ if ($_SERVER['HTTP_HOST']=='192.168.1.11') {
 
 $mysql_con=mysql_connect($db_host, $db_username, $db_password) or die (mysql_error ());
 mysql_select_db($db_name,$mysql_con) or die(mysql_error());
+*/
+
+$db_connection=new database();
