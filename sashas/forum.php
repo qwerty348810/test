83d65a63c6ac347c606d1b6f1c17cce16db4a519
Alex Lukyanau <cybergomel@gmail.com>
2014-08-13 00:41:13 -0400

    parent logic

--- sashas/forum.php
+++ sashas/forum.php
@@ -1,6 +1,8 @@
 <?php
 
-$sql="SELECT * FROM topic WHERE parent_id=0";
+$parent_id=(isset($_GET['parent_id']) ? $_GET['parent_id'] : 0);
+
+$sql="SELECT * FROM topic WHERE parent_id=".$parent_id;
 
 $result=mysql_query($sql,$mysql_con);
 
@@ -10,6 +12,6 @@ while ($row = mysql_fetch_assoc($result)) {
  	array_push($topics, $row);
 }
 
-
+$action='forum';
 
 include_once $action.'.phtml';
