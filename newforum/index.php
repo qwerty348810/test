f714d90e040b99370902ff4333f654fda7d87709
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-14 06:26:38 +0200

    try2

--- newforum/index.php
+++ newforum/index.php
@@ -1,16 +1,20 @@
 <?php
-error reporning (E_ALL);
-ini_set ('display error',1);
-session start ();
-include_once 'template/header.phtml';
+error_reporting(E_ALL);
+ini_set('display_reporting',1);
+session_start();
+
+include_once 'template/header.phtm';
 
 include_once 'variables.php';
 
 $action="forum";
 
-if(isset($_GET['action'])){
-    $action=$_get['action'];
+if (isset($_GET['action'])){
+    $action=$_GET['action'];
 }
 
-include_once 'footer.phtml';
+include_once $action.'.php';
+
+include_once 'template/foooter.phtml';
+
 ?>
