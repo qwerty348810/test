1bddf31287243d6fd6469aa7ff2b96f12a67a1e2
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-14 07:44:44 +0200

    try4

--- newforum/index.php
+++ newforum/index.php
@@ -1,19 +1,18 @@
 <?php
 error_reporting(E_ALL);
-ini_set('display_error',1);
+ini_set('display',1);
 session_start();
 
 include_once 'template/header.phtml';
 
 include_once 'variables.php';
 
-$action="forum";
+$action='forum';
 
 if (isset($_GET['action'])){
     $action=$_GET['action'];
 }
-
 include_once $action.'.php';
 
-include_once 'template/footer,phtml';
+include_once 'template/footer.phtml';
 ?>
