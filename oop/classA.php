92ba2fe1e781e5d5690d9298c5c5ad0cbc5604dc
Alex Lukyanau <cybergomel@gmail.com>
2014-08-17 07:02:49 -0400

    OOP

--- oop/classA.php
+++ oop/classA.php
@@ -2,11 +2,13 @@
 
 class Electronics  {
 	
-	 private $_height;
-	 private $_width;
-	 private $_weight;
+	 private $_height=1;
+	 private $_width=2;
+	 private $_weight=3;
 	 
-	 
+	 public function getParameters(){
+	 	return $this->_height."x".$this->_width.'x'.$this->_weight;
+	 }
 	 
 
 	
--- oop/index.php
+++ oop/index.php
@@ -2,3 +2,10 @@
 include_once 'classA.php';
 include_once 'classB.php';
 include_once 'PC.php';
+
+$electronics= new Electronics();
+$tv= new TV();
+$pc=new PC();
+
+
+echo $electronics->getParameters();
