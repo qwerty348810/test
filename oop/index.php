f3df2329f87558714ed75ff4069e5f390adea0ec
Alex Lukyanau <cybergomel@gmail.com>
2014-08-17 06:56:02 -0400

    OOP

--- oop/index.php
+++ oop/index.php
@@ -1,20 +1,4 @@
 <?php
 include_once 'classA.php';
 include_once 'classB.php';
-
-
-$arr=array(5,2,1,2,3,4,5,6,7,3);
-$arr2=array(1,1,1,1,1,1,1,1,1,1);
-
-$a_class=new Aclass();
-
-
-//$result=$a_class->summ(5, 7);
-
-
-for  ($i=0; $i<count($arr);$i++) {
-	$result[]=$a_class->summ($arr[$i], $arr2[$i]);
-}
-
-
-print_r($result);
+ 
