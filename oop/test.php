15f210f065cfd584a91fa36cb96ab44dd8debf97
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-18 05:59:45 +0200

    test oop

---
+++ oop/test.php
@@ -0,0 +1,20 @@
+<?php
+class test {
+    function base() {
+      echo "<h1>Это базовый класс<br>";  
+    }
+    function base_test(){
+        $this->base;
+    }
+}
+class inc_test extends test{
+    function base_test(){
+        echo "Это производный класс";
+    }
+}
+$a=new test();
+$b=new inc_test();
+$a->base();
+$b->base_test();
+
+?>
