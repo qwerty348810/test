3c2caa17b55c89240ee1452d1e5a8a377159248c
Dima <Dmitrii.yakovlev@mail.ru>
2014-08-19 03:28:10 +0200

    correction2

--- newforum/messages.php
+++ newforum/messages.php
@@ -1,19 +1,19 @@
 <?php
-$messages_added='';
+$message_added='';
 
 if(isset($_POST['parent_id'])){
     $post_parent_id=$_POST['parent_id'];
     $post_messages_text=$_POST['messages_text'];
-    $sql="INSERT INTO messages (messages,parent_id) VALUE ('".$post_parent_id."','".$post_messages_text."')";
+    $sql="INSERT INTO messages (messages,parent_id) VALUE ('".$post_messages_text."','".$post_parent_id."')";
     $db_connection->save($sql);
     $messages_added="<center>Messages Added!Thanks</center>";
 }
 $parent_id=(isset($_GET['parent_id']) ? ($_GET['parent_id']): 0);
-$sql="SELECT * FROM messages WHERE parent_id='.$parent_id.'";
-$result=$db_connection->query($sql);
-$messages='forum';
+$sql="SELECT * FROM messages WHERE parent_id=".$parent_id;
+$result=mysql_query($sql);
+$message=array();
 while ($row=mysql_fetch_assoc($result)){
-    array_push($messages,$row);
+    array_push($message,$row);
 }
-include_once $template.$action.'.phtml';
+include_once $template_dir.$action.'.phtml';
 ?>
