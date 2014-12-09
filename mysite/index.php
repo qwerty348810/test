816563550dff6937a89d72f5894b9e57eb7393a1
qwerty348810 <Dmitrii.yakovlev@mail.ru>
2014-12-09 11:14:14 +0200

    load

--- mysite/index.php
+++ mysite/index.php
@@ -4,15 +4,6 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
-    <head>
-        <title></title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="css/style.css" type="text/css"/>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    </head>
-    <body>
-        <div>TODO write content</div>
-    </body>
-</html>
+<?php include_once 'header.phtml'; ?>
+<?php include_once 'page.phtml'; ?>
+<?php include_once 'footer.phtml'; ?>
